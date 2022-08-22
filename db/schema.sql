a8c18c59b49f14e06a46763c2824890b13cd3269
Northern Pines <traviswalborn8@gmail.com>
2022-08-21 22:38:56 -0400

    view functions added

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE departments (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
-)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
 
 CREATE TABLE employees (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id
-)
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
--- index.js
+++ index.js
@@ -6,43 +6,83 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: 'rootbeer',
-        database: 'emp-data'
+        database: 'employees_db'
     },
-    console.log(`Connected to the movies_db database.`)
+    console.log(`Connected to the employees_db database.`)
 );
+db.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+    question();
+})
+function question() {
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: "What would you like to do",
-            name: 'selection',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee role']
-        }
-    ])
-    .then((data) => {
-        const choice = data.selection;
-        switch (choice) {
-            case 'View all departments':
-                viewDepartments();
-                break;
-            case 'View all roles':
-                viewRoles();
-                break;
-            case 'View all employees':
-                viewEmployees();
-                break;
-            case 'Add a department':
-                addDepartments();
-                break;
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update employee role':
-                updateRole();
-                break;
-        }
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: "What would you like to do",
+                name: 'selection',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee role']
+            }
+        ])
+        .then((data) => {
+            const choice = data.selection;
+            switch (choice) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update employee role':
+                    updateRole();
+                    break;
+            }
+        })
+}
+
+
+function viewDepartments() {
+    db.query('SELECT * FROM departments', function(err, data) {
+        if (err) {
+            throw err;
+        } 
+        console.table(data);
+        question();
     })
+}
+
+function viewRoles() {
+    db.query('SELECT * FROM roles', function(err, data) {
+        if (err) {
+            throw err;
+        } 
+        console.table(data);
+        question();
+    })
+}
+
+function viewEmployees() {
+    db.query('SELECT * FROM employees', function(err, data) {
+        if (err) {
+            throw err;
+        } 
+        console.table(data);
+        question();
+    })
+}
+
